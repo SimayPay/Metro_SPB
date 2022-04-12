006747d7c11480fc3995451bc193d0fdd697ce1f
pilat <pilat@DESKTOP-MQ2MFDN>
2022-04-12 20:06:15 +0300

    Добавьте файлы проекта.

---
+++ lab_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+
+struct station
+{
+	station* previous;
+	station* next;
+};
+
+vector <station> Stations;
+
+int main()
+{
+	station* s1 = new station();
+	
+}
